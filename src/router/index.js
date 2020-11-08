8762c27cf0f6506e83385e03ada508be4ffc5446
axnir <lxgjzyj@hotmail.com>
2020-11-08 19:44:41 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -2,34 +2,21 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 // 使用路由懒加载
-// import Login from '../components/Login.vue'
 const Login = () => import(/* webpackChunkName: "login_home_welcome" */ '../components/Login.vue')
-// import Home from '../components/Home.vue'
 const Home = () => import(/* webpackChunkName: "login_home_welcome" */ '../components/Home.vue')
-// import Welcome from '../components/Welcome.vue'
 const Welcome = () => import(/* webpackChunkName: "login_home_welcome" */ '../components/Welcome.vue')
 
-// import Users from '../components/user/Users.vue'
 const Users = () => import(/* webpackChunkName: "users_rights_roles" */ '../components/user/Users.vue')
-// import Rights from '../components/power/Rights.vue'
 const Rights = () => import(/* webpackChunkName: "users_rights_roles" */ '../components/power/Rights.vue')
-// import Roles from '../components/power/Roles.vue'
 const Roles = () => import(/* webpackChunkName: "users_rights_roles" */ '../components/power/Roles.vue')
 
-// import Cate from '../components/goods/Cate.vue'
 const Cate = () => import(/* webpackChunkName: "cate_params" */ '../components/goods/Cate.vue')
-// import Params from '../components/goods/Params.vue'
 const Params = () => import(/* webpackChunkName: "cate_params" */ '../components/goods/Params.vue')
 
-// import GoodsList from '../components/goods/List.vue'
 const GoodsList = () => import(/* webpackChunkName: "goodlist_goodsadd" */ '../components/goods/List.vue')
-// import GoodsAdd from '../components/goods/Add.vue'
 const GoodsAdd = () => import(/* webpackChunkName: "goodlist_goodsadd" */ '../components/goods/Add.vue')
 
-// import Order from '../components/order/Order.vue'
 const Order = () => import(/* webpackChunkName: "order" */ '../components/order/Order.vue')
-
-// import Report from '../components/report/Report.vue'
 const Report = () => import(/* webpackChunkName: "report" */ '../components/report/Report.vue')
 
 Vue.use(VueRouter)
